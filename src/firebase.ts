6be8db97dce0362ae6a8b931afbbb727f615daba
plou102 <plou102@naver.com>
2023-10-29 21:37:18 +0900

    Feat: firebase install & initialization

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD05MLpjV1nnGpnB5P3zg1yNgPyvwVQWgY",
+  authDomain: "fir-practice-5023f.firebaseapp.com",
+  projectId: "fir-practice-5023f",
+  storageBucket: "fir-practice-5023f.appspot.com",
+  messagingSenderId: "53308825017",
+  appId: "1:53308825017:web:66d35a4b34c9b0abc22a68",
+};
+
+const app = initializeApp(firebaseConfig);
