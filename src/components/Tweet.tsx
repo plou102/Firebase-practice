e34ea3c05c1d93ecafd06c92f336f4d30deb9bb4
plou102 <plou102@naver.com>
2023-11-09 17:23:01 +0900

    Feat: create delete tweet

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { ITweet } from "./Timeline";
 import styled from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 
-export default function Tweet({ username, photo, tweet }: ITweet) {
+export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const user = auth.currentUser;
+  async function onDelete() {
+    const checkMessage = confirm("이 트윗을 삭제하시겠습니까?");
+
+    if (!checkMessage || user?.uid !== userId) return;
+
+    try {
+      await deleteDoc(doc(db, "tweets", id));
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+        await deleteObject(photoRef);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        {user?.uid === userId ? (
+          <DeleteBtn onClick={onDelete}>Delete</DeleteBtn>
+        ) : null}
       </Column>
 
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
@@ -40,4 +63,16 @@ const Photo = styled.img`
   height: 100px;
   border-radius: 15px;
   float: right;
+`;
+
+const DeleteBtn = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 50px;
+  cursor: pointer;
 `;
