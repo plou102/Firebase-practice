7360133ae0ae6ab2812b0c80224f7883139ba276
plou102 <plou102@naver.com>
2023-11-15 15:55:46 +0900

    Feat: create user's tweets

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
-import { auth, storage } from "../firebase";
+import React, { useEffect, useState } from "react";
+import { auth, db, storage } from "../firebase";
 import styled from "styled-components";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { ITweet } from "../components/Timeline";
+import Tweet from "../components/Tweet";
 
 export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL);
+  const [tweets, setTweets] = useState<ITweet[]>([]);
 
   async function onAvatarChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { files } = e.target;
@@ -25,6 +36,27 @@ export default function Profile() {
       });
     }
   }
+
+  async function fetchTweets() {
+    const tweetQuery = query(
+      collection(db, "tweets"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+      limit(25)
+    );
+
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return { tweet, createdAt, userId, username, photo, id: doc.id };
+    });
+
+    setTweets(tweets);
+  }
+
+  useEffect(() => {
+    fetchTweets();
+  }, []);
 
   return (
     <Wrapper>
@@ -51,6 +83,11 @@ export default function Profile() {
       />
 
       <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
     </Wrapper>
   );
 }
@@ -87,4 +124,11 @@ const AvatarInput = styled.input`
 
 const Name = styled.span`
   font-size: 22px;
+`;
+
+const Tweets = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
 `;
