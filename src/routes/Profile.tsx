0f0c12df171be1c2396a6c90b32597c66e97e9b7
plou102 <plou102@naver.com>
2023-11-22 18:04:12 +0900

    Feat: create edit profile displayname

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -18,6 +18,8 @@ export default function Profile() {
   const user = auth.currentUser;
   const [avatar, setAvatar] = useState(user?.photoURL);
   const [tweets, setTweets] = useState<ITweet[]>([]);
+  const [name, setName] = useState(user?.displayName);
+  const [isEdit, setIsEdit] = useState(false);
 
   async function onAvatarChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { files } = e.target;
@@ -58,6 +60,25 @@ export default function Profile() {
     fetchTweets();
   }, []);
 
+  function onNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  }
+
+  async function onEdit() {
+    if (!isEdit) {
+      setIsEdit(true);
+      return;
+    }
+
+    if (!user) return;
+
+    await updateProfile(user, {
+      displayName: name,
+    });
+
+    setIsEdit(false);
+  }
+
   return (
     <Wrapper>
       <AvatarUpload htmlFor="avatar">
@@ -82,7 +103,41 @@ export default function Profile() {
         accept="image/*"
       />
 
-      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <NameWrapper>
+        {isEdit ? (
+          <NameInput type="text" onChange={onNameChange} />
+        ) : (
+          <Name>{user?.displayName ?? "Anonymous"}</Name>
+        )}
+
+        <NameEditBtn onClick={onEdit}>
+          {isEdit ? (
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                clipRule="evenodd"
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+              />
+            </svg>
+          ) : (
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+              <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+            </svg>
+          )}
+        </NameEditBtn>
+      </NameWrapper>
+
       <Tweets>
         {tweets.map((tweet) => (
           <Tweet key={tweet.id} {...tweet} />
@@ -122,8 +177,33 @@ const AvatarInput = styled.input`
   display: none;
 `;
 
+const NameWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+  margin-bottom: 10px;
+  height: 35px;
+  line-height: 35px;
+`;
+
 const Name = styled.span`
   font-size: 22px;
+`;
+
+const NameInput = styled.input`
+  background-color: transparent;
+  border: 1px solid white;
+  border-radius: 10px;
+  color: white;
+  font-size: 20px;
+  width: 150px;
+  padding: 5px 8px;
+`;
+
+const NameEditBtn = styled.span`
+  svg {
+    width: 22px;
+    height: 35px;
+  }
 `;
 
 const Tweets = styled.div`
